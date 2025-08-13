effb46ae5a3f8d2b3318a31ae4fb6db523bff51a
Ken Ranosa <ken.ranosa@gmail.com>
2025-08-13 21:22:52 +0800

    Updated command colors and description

--- components/terminal/commands/HelpCommand.tsx
+++ components/terminal/commands/HelpCommand.tsx
@@ -3,19 +3,19 @@ export function HelpCommand() {
     <div className="space-y-1">
       <div className="solar-yellow ultra-thin">Available commands:</div>
       <div className="ultra-light">
-        <span className="quantum-blue">work</span> - Career highlights teaser (click to see full experience)
-      </div>
-      <div className="ultra-light">
-        <span className="cyber-purple">blueprint</span> - My karmic blueprint via vedic astrology
-      </div>
-      <div className="ultra-light">
         <span className="nebula-pink">about</span> - About this terminal
+      </div>
+      <div className="ultra-light">
+        <span className="nebula-pink">blueprint</span> - My karmic blueprint via vedic astrology
       </div>
       <div className="ultra-light">
         <span className="nebula-pink">clear</span> - Clear terminal
       </div>
       <div className="ultra-light">
-        <span className="cyber-purple">reload</span> - Reload with splash screen
+        <span className="nebula-pink">reload</span> - Reload with splash screen
+      </div>
+      <div className="ultra-light">
+        <span className="nebula-pink">work</span> - My comprehensive professional background
       </div>
     </div>
   )
